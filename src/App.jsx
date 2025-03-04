82e4bcae3a7f1a9e1ff0149b7c2225a300313bc8
Naman1369 <naman.binu.s73@kalvium.community>
2025-03-04 14:46:55 +0530

    ca completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TestimonialCard from './components/TestimonialCard'
+
+function App() {
+  return(
+    <div>
+      <TestimonialCard/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TestimonialCard() {
+    return(
+        <div style={{textAlign: "left"}}>
+            <p>Customer 1: Vraj Patel</p>
+            <p >Review: This service was really amazing!</p>
+            <p>Customer 2: Shriraj Jadhav</p>
+            <p>Review: This service was ver helpful! Highly recommended</p>
+        </div>
+        
+    )
+
+};
+
+export default TestimonialCard;
